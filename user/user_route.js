a307b97fb17ba5b36192363e2e05308602576449
Peeranat <peeranat1412@gmail.com>
2019-06-30 19:41:31 +0700

    fix es 2016

--- user/user_route.js
+++ user/user_route.js
@@ -1,22 +1,24 @@
-import { authenticate, createUser, getAllUser, updateLesson, updatePassword, geUserById } from './user_controller';
-import { Router } from 'express';
-import { checkAuthenticate, checkAuthenticateUpdate } from '../authenticate';
-var router = Router();
+
+
+const user_controller = require('./user_controller');
+const app = require('express');
+const authenticateControl = require('../authenticate');
+var router = app.Router();
 
 router.post('/authenticate' ,login);
 router.post('/register', register);
-router.get('/list', checkAuthenticate ,getall);
+router.get('/list', authenticateControl.checkAuthenticate ,getall);
 router.get('/getUserById/:id' ,getUser);
 router.put('/updatePass/:id', updateUser);
 router.put('/updatelesson/:id', updateLess)
 function login(req, res, next){
-    authenticate(req.body)
+    user_controller.authenticate(req.body)
     .then(user=> user ? res.json(user) : res.status(400).json({message : "username and password incorrect"}))
     .catch(err => next(err));
 }
 
 function register(req, res, next){
-    createUser(req.body).then(user => {  
+    user_controller.createUser(req.body).then(user => {  
         if(user) {res.status(200).json(user);}
         else{
         console.log("Cant update database");
@@ -30,7 +32,7 @@ function register(req, res, next){
 
 
 function getUser(req,res, next){
-    geUserById(req.params.id).then(user => {  
+    user_controller.geUserById(req.params.id).then(user => {  
         if(user) {res.status(200).json(user);}
         else{
         console.log("Cant get user profile");
@@ -43,17 +45,17 @@ function getUser(req,res, next){
 }
 
 function getall(req,res, next){
-    getAllUser().then(user=> user ? res.json(user) : res.status(400).json({mess : "Error while get all user"}))
+    user_controller.getAllUser().then(user=> user ? res.json(user) : res.status(400).json({mess : "Error while get all user"}))
     .catch(err => next(err));
 }
 
 function updateUser(req,res,next){
-    updatePassword(req.params.id, req.body)
+    user_controller.updatePassword(req.params.id, req.body)
     .then(user=> user ? res.json(user) : res.status(400).json({message : "Cant update database"}))
     .catch(e => {res.status(400).json(e);})
 }
 function updateLess(req,res,next){
-    updateLesson(req.params.id, req.body).then(user => {  
+    user_controller.updateLesson(req.params.id, req.body).then(user => {  
         if(user) {res.status(200).json(user);}
         else{
         console.log("Cant update database");
